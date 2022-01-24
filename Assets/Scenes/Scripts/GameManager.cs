541b88982afb59cd844ac4f45b2ddbfce23a340b
mja123 <matias.aguilar.sansoni@gmail.com>
2022-01-24 11:42:04 -0300

    Un solo salto permitido y pantalla de inicios y fin agregadas

--- Assets/Scenes/Scripts/GameManager.cs
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject startMenu;
+    public GameObject gameOverMenu;
+
     public GameObject Superficie;
     public Renderer fondo;
     public List<GameObject> cols;
     public float velocidad = 2;
     public int incrementoX;
+    public bool startGame = false;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,35 +26,56 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        fondo.material.mainTextureOffset += new Vector2(0.015f, 0) * Time.deltaTime;
-        
-        for (int i = 0; i < cols.Count; i++)
-        {
-            
-            incrementoX = Random.Range(3, 6);
-            if (cols[i].transform.position.x <= -10.35f)
-            {
-                cols[i].transform.position = new Vector3(10.35f, -2, 0);
-            
-                //cuando i es divisible por 3 y con un 20% de posibilidad de entrar
-                //se crea un hueco de entre 3 y 6 pixeles, lo que asegura que siempre haya aunque sea una parte de piso para caer y
-                // no hayan huecos de menos de 2 pixeles
-                
-                if (i % 3 == 0 && Random.Range(1, 10) >= 4) 
-                {
-                    cols[i].transform.position = new Vector3(10.35f + incrementoX, -2, 0);
-                }
-                
-                velocidad += 0.01f;
-            }
-            
+    { 
 
-            
-            //movimiento del suelo
-            
-            cols[i].transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * (velocidad);
-            
+        if (!startGame)
+        {           
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                startGame = true;
+            }
+        }
+
+        if (startGame & gameOver)
+        {
+            gameOverMenu.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        if (startGame & !gameOver)
+        {
+            startMenu.SetActive(false);
+            fondo.material.mainTextureOffset += new Vector2(0.015f, 0) * Time.deltaTime;
+            for (int i = 0; i < cols.Count; i++)
+            {
+                
+                incrementoX = Random.Range(3, 6);
+                if (cols[i].transform.position.x <= -10.35f)
+                {
+                    cols[i].transform.position = new Vector3(10.35f, -2, 0);
+
+                    //cuando i es divisible por 3 y con un 20% de posibilidad de entrar
+                    //se crea un hueco de entre 3 y 6 pixeles, lo que asegura que siempre haya aunque sea una parte de piso para caer y
+                    // no hayan huecos de menos de 2 pixeles
+
+                    if (i % 3 == 0 && Random.Range(1, 10) >= 4)
+                    {
+                        cols[i].transform.position = new Vector3(10.35f + incrementoX, -2, 0);
+                    }
+
+                    velocidad += 0.05f;
+                }
+
+
+
+                //movimiento del suelo
+
+                cols[i].transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * (velocidad);
+
+            }
         }
     }
 }
--- Assets/Scenes/Scripts/Jugador.cs
+++ Assets/Scenes/Scripts/Jugador.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class Jugador : MonoBehaviour
 {
     public float fuerzaSalto;
-    private Rigidbody2D rigidbody2D;
-    private Animator animator;
+    public Rigidbody2D rigidbody2D;
+    public Animator animator;
+    public GameManager gameManager;
+    public bool saltando;   
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();        
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
+    {        
+
+        if(Input.GetKeyDown(KeyCode.Space) & !saltando)
         {
             animator.SetBool("saltando", true);
             rigidbody2D.AddForce(new Vector2(0, fuerzaSalto));
             animator.SetBool("corriendo", false);
+            saltando = true;
         }
+
+        if(animator.transform.position.y < -4)
+        {            
+            gameManager.gameOver = true;
+        }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -30,6 +40,7 @@ public class Jugador : MonoBehaviour
         {
             animator.SetBool("saltando", false);
             animator.SetBool("corriendo", true);
+            saltando = false;
         }
     }
 }
